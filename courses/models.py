6b2e6988d7eaa713bb3ab69abe153a67ec580c54
Bhavesh <bhaveshpraveen10@gmail.com>
2017-12-14 16:31:41 +0530

    Add Content, File, Image, Text Model.

--- courses/models.py
+++ courses/models.py
@@ -1,4 +1,6 @@
 from django.conf import settings
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 from django.db import models
 
 
@@ -27,6 +29,7 @@ class Course(models.Model):
     def __str__(self):
         return self.title
 
+
 class Module(models.Model):
     course = models.ForeignKey(Course, related_name='modules')
     title = models.CharField(max_length=200)
@@ -34,5 +37,44 @@ class Module(models.Model):
 
     def __str__(self):
         self.title
+
+
+class ItemBase(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='%(class)s_related')
+    title = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title
+
+
+class Text(ItemBase):
+    content = models.TextField()
+
+
+class File(ItemBase):
+    file = models.FileField(upload_to='files')
+
+
+class Image(ItemBase):
+    file = models.FileField(upload_to='files')
+
+
+class Video(ItemBase):
+    url = models.URLField()
+
+
+class Content(models.Model):
+    module = models.ForeignKey(Module, related_name='contents')
+
+    content_type = models.ForeignKey(ContentType,
+                                     limit_choices_to={'model_in': ('text', 'video', 'image', 'file')}
+                                     )
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type', 'object_id')
 
 
--- educa/settings.py
+++ educa/settings.py
@@ -31,24 +31,24 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'courses',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'courses',
 ]
-
-MIDDLEWARE = [
+MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
 ]
+
 
 ROOT_URLCONF = 'educa.urls'
 
